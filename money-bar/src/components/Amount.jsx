157f61631306975cdcf81396061c71181995cc64
Spencer Ng <spencerng94@gmail.com>
2021-05-13 14:07:37 -0700

    Implemented styling changes for hovering over amount

--- money-bar/src/components/Amount.jsx
+++ money-bar/src/components/Amount.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function separator(numb) {
     var str = numb.toString().split(".");
@@ -9,13 +9,33 @@ function separator(numb) {
 const Amount = (props) => {
 
     const { amount } = props;
+    const [ isEditable, toggleEditable ] = useState(false);
+    const [ userText, editText ] = useState({amount});
+    const [ isHovered, toggleHover ] = useState(false);
+    const [ isSelected, toggleSelected ] = useState(false);
     
     let newAmount = separator(amount);
 
+    let borderStyle = {
+        border: ''
+    };
+
+    if (isHovered) {
+        borderStyle = {
+            border: '1px solid #E0E0E3'
+        }
+    }
+
     return (
-        <div className="amount-container">
+        <div style={borderStyle} onMouseEnter={() => toggleHover(true)}
+        onMouseLeave={() => toggleHover(false)} className="amount-container">
             <div className="amount">
-                ${newAmount}
+                { isEditable ? <input type="text" value={amount}/>
+                    :
+                    <div>
+                        ${newAmount}
+                    </div>
+                }
             </div>
         </div>
     )
